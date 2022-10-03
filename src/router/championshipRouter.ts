4635e6feb2d6af4080248c80176693cd1b9399f1
Kadson Lima <kadson-kaio@hotmail.com>
2022-10-03 14:36:21 -0300

    chore: scripts start & build

--- src/router/championshipRouter.ts
+++ src/router/championshipRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { create, getChampionshipById, getChampionships } from "../controllers/championshipController";
+import { validateJWT } from "../middlewares/validateJwt";
 
 const championshipRouter = Router()
 
-
+championshipRouter.use(validateJWT())
 championshipRouter.post('/championship', create);
 championshipRouter.get('/championship', getChampionships);
 championshipRouter.get('/championship/:id', getChampionshipById);
